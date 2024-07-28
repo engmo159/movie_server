1881a476b42e5b68f60bb4d04a8f5db79fe91742
Mohammed <engmo159@git.com>
2024-07-29 00:56:04 +0300

    rfr

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ import routes from './src/routes/index.js'
 
 const app = express()
 
-// app.use(cors())
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
